33f53ff92ee03cfa22677b50057f8e8ecf3666b5
Renato Lima <renatosqlima@gmail.com>
2026-07-08 21:25:54 -0300

    updateLoginControllerAndConfiguracoes

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -3,11 +3,13 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -28,5 +30,20 @@ public class ConfiguracoesSeguranca {
                 .password("{noop}iasmin123")
                 .build();
         return new InMemoryUserDetailsManager(usuario1, usuario2, usuario3);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(req -> {
+                    req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                    req.anyRequest().authenticated();
+                })
+
+                .formLogin(form -> form.loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout")
+                        .permitAll())
+                .build();
     }
 }
